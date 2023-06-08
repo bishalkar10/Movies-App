31ae3a030a78df075fc51d89ed63bd760102fc5e
Bishal Kar <bishalkar10@gmail.com>
2023-06-08 18:50:56 +0530

    removed an import

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,6 +1,5 @@
 import Trending from "../components/Trending";
 import Discover from "../components/Discover";
-import SearchResults from "../components/SearchResults";
 
 export default function Homepage() {
   return (
