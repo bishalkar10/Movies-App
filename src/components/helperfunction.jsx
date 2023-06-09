2d745d475ff1b2cc0daa993c1cc42ae593fa54fb
Bishal Kar <bishalkar10@gmail.com>
2023-06-09 16:09:33 +0530

    implemented useScrollVIsibilty custom Hook and ScrollToTopButton component

--- src/components/helperfunction.jsx
+++ src/components/helperfunction.jsx
@@ -1,7 +1,28 @@
+import { useEffect, useState } from "react";
+
+export function useScrollVisibility() {
+  const scrollThreshold = 200; // Adjust this value as needed
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    function handleScroll() {
+      setIsVisible(window.scrollY > scrollThreshold);
+    }
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return isVisible;
+}
+
 export function getReleaseYear(release_date) {
   const date = new Date(release_date);
   return date.getFullYear();
 }
+
 export function getRuntime(runtime) {
   runtime = parseInt(runtime);
   const hour = Math.floor(runtime / 60); // if runtime is 176m  then hour is 176/ 60 = 2
@@ -21,9 +42,21 @@ export function formatDate(dateString) {
 
   return formattedDate; // Output: Mar 22, 2023
 }
-export function scrollToTop() {
+
+function scrollToTop() {
   window.scrollTo({
     top: 0,
     behavior: "smooth",
   });
+}
+// scroll to top arrow button component
+export function ScrollToTopButton() {
+  return (
+    <div
+      onClick={scrollToTop}
+      className="fixed grid w-8 h-8 bg-green-800 text-white rounded-full shadow-[0_10px_10px_2px_rgba(0,0,0,0.3)] cursor-pointer bottom-5 right-5 place-content-center animate-bounce"
+    >
+      <i className="fa fa-arrow-up"></i>
+    </div>
+  );
 }
